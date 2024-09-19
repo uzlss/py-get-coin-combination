525eb1f428865b4edc1c1266156ce1a2e1c834da
Ihor Bachuk <igor.bachuk04@gmail.com>
2024-09-19 05:09:00 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,29 @@
+import pytest
+
 from app.main import get_coin_combination
 
 
-# write your tests here
+@pytest.mark.parametrize(
+    "cents, result",
+    [
+        (0, [0, 0, 0, 0]),
+        (1, [1, 0, 0, 0]),
+        (5, [0, 1, 0, 0]),
+        (10, [0, 0, 1, 0]),
+        (25, [0, 0, 0, 1]),
+        (42, [2, 1, 1, 1])
+    ],
+    ids=[
+        "Zero cents returns list of zeros",
+        "1 cent adds one pennie",
+        "5 cents add one nickel",
+        "10 cents add one dime",
+        "25 cents add one quarter",
+        "All cents should be converted"
+    ]
+)
+def test_get_coin_combination(
+        cents: int,
+        result: list[int]
+) -> None:
+    assert get_coin_combination(cents) == result
